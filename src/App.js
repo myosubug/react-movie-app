5fe53ced85608fdf24359d6218032f00c05d4ebb
sean <seanoh.dev@gmail.com>
2020-09-25 22:28:08 -0600

    learned how to pass argument with jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+function Food({fav}){
+  return <h1>I like {fav}</h1>
+}
+
+
 function App() {
   return (
     <div className="App">
       Hellooo!! 
+      <Food fav = "Kimchi" />
+      <Food fav = "Gookbob" />
+      <Food fav = "Bread" />
+      <Food fav = "Ramen" />
+
     </div>
   );
 }
